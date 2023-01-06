25a6186bc3191c441ef947e124ba21213b80a129
Briexdon <arcticdeutsch@gmail.com>
2023-01-06 19:53:08 +0200

    start

---
+++ src/Components/main-page/header/header-main-page.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+import "./header-main-page.css";
+
+class HeaderMainPage extends Component {
+  render() {
+    return (
+      <>
+        <h1>hello World</h1>
+      </>
+    );
+  }
+}
+
+export default HeaderMainPage;
---
+++ src/Components/main-page/main-page.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import HeaderMainPage from "./header/header-main-page";
+
+class MainPage extends Component {
+  render() {
+    return (
+      <>
+        <HeaderMainPage></HeaderMainPage>
+      </>
+    );
+  }
+}
+
+export default MainPage;
