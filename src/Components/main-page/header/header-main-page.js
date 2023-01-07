cdbd934052859a648b063acad078525f81ee8ce8
Briexdon <arcticdeutsch@gmail.com>
2023-01-07 09:04:20 +0200

    header main page

--- src/Components/main-page/header/header-main-page.js
+++ src/Components/main-page/header/header-main-page.js
@@ -2,13 +2,36 @@ import { Component } from "react";
 
 import "./header-main-page.css";
 
+import headerBg from "../../../icons/main-page/header-bg.jpg";
+import coffeeBeans from "../../../icons/main-page/coffee-beans.png";
+import whiteBeansLogo from "../../../icons/main-page/beans-logo.png";
+
 class HeaderMainPage extends Component {
   render() {
     return (
-      <>
-        <h1>hello World</h1>
-        <h2>hello again</h2>
-      </>
+      <header className="header-main-page">
+        <img src={headerBg} alt="header-bg" />
+        <nav className="header-pages">
+          <div className="coffee-house">
+            <img src={coffeeBeans} alt="coffee-beans_white" />
+            <a href="#">Coffee house</a>
+          </div>
+          <div className="our-coffee">
+            <a href="#">Our coffee</a>
+          </div>
+          <div className="for-your-pleasure">
+            <a href="#">For your pleasure</a>
+          </div>
+        </nav>
+        <div className="header-descr">
+          <div className="header-title">Everything You Love About Coffee</div>
+          <img src={whiteBeansLogo} alt="white-beans-logo" />
+          <div className="header-text">
+            We makes every day full of energy and taste <br /> Want to try our beans?
+          </div>
+          <button className="header-btn">More</button>
+        </div>
+      </header>
     );
   }
 }
